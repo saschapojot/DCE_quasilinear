b794d9f739795c34371273192d3edab94ddbee10
Xi LIU <xi.liu@u.nus.edu>
2024-11-15 20:25:23 +0800

    example

--- fftw_example.cpp
+++ fftw_example.cpp
@@ -22,3 +22,30 @@ void fft_rows(std::complex<double>* input, std::complex<double>* output, int N1,
     fftw_execute(plan);
     fftw_destroy_plan(plan);
 }
+
+void fft_columns ( std :: complex < double >* input , std :: complex < double >* output , int N1 , int N2 ){
+// Define FFT parameters for column - wise FFT
+    int rank = 1; // 1 D FFT
+    int n [] = { N1 }; // Length of each FFT is N1 ( number of rows )
+    int howmany = N2 ; // Number of FFTs is N2 ( number of columns )
+    int istride = N2 , ostride = N2 ; // Stride for column - wise FFT
+    int idist = 1 , odist = 1; // Distance between start of each column FFT
+
+    // Create FFTW plan for column - wise , out - of - place transform
+
+    fftw_plan plan = fftw_plan_many_dft (
+            rank , n , howmany ,
+            reinterpret_cast < fftw_complex * >( input ) , NULL ,
+            istride , idist ,
+            reinterpret_cast < fftw_complex * >( output ) , NULL ,
+            ostride , odist ,
+            FFTW_FORWARD , FFTW_ESTIMATE
+    ) ;
+
+    // Execute the FFT plan
+    fftw_execute ( plan ) ;
+
+    // Clean up
+    fftw_destroy_plan ( plan ) ;
+
+}
